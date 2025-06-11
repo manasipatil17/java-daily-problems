e0d3702e3ebe880af257ae647970fd0ce6c44a6f
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-11 21:28:06 +0530

    added ascii.java

---
+++ src/com/string/Ascii.java
@@ -0,0 +1,10 @@
+package com.string;
+
+public class Ascii {
+public static void main(String[] args) {
+	int ascii[] = {65, 66, 67, 68}; 
+    String s = new String(ascii, 1, 3);
+    System.out.println(s);
+    
+}
+}
