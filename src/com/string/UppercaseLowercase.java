250457c745f33877b9fc1ec94f32f0898c2a9473
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-16 12:14:51 +0530

    added UppercaseLowercase.java

---
+++ src/com/string/UppercaseLowercase.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class UppercaseLowercase {
+public static void main(String[] args) {
+	String s="India";
+	System.out.println(s.toUpperCase());
+	System.out.println(s.toLowerCase());
+}
+}
