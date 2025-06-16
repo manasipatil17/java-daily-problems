a81bf5bda7c7eda53fccdb21f8560b40dd8fa006
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-16 12:11:36 +0530

    added ReverseString.java

---
+++ src/com/string/ReplaceChar.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class ReplaceChar {
+	//Write a Java program to replace all occurrences of a character with another.
+
+	public static void main(String[] args) {
+		String s="satara";
+	}
+}
