027337fcd65d7cc921d5635c06c97b4f01c9a58f
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-16 12:22:44 +0530

    added LargestElement.java

---
+++ src/com/array/LargestElement.java
@@ -0,0 +1,18 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class LargestElement {
+public static void main(String[] args) {
+	int a[]= {12,37,32,44,65,11};
+	int largest =a[0];
+	
+	for(int i=0;i<a.length;i++) {
+		if(a[i]>largest) {
+			largest=a[i];
+		}
+	}
+	System.out.println(Arrays.toString(a));
+	System.out.println("Largest element from array : "+largest);
+}
+}
