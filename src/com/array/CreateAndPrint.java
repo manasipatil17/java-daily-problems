07af7588e45ec1ff0671fa171ab564f0c0220448
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-19 20:52:41 +0530

    Added CreateAndPrint.java

---
+++ src/com/array/CreateAndPrint.java
@@ -0,0 +1,19 @@
+package com.array;
+
+import java.util.Arrays;
+
+public class CreateAndPrint {
+	//create array of 5 integers and print all of elements.
+		public static void main(String[] args) {
+			
+	       //int a[]= {10,12,33,44,55};
+	       int a[]=new int[5];
+	       a[0]=10;
+	       a[1]=12;
+	       a[2]=33;
+	       a[3]=44;
+	       a[4]=55;
+	       
+	       System.out.println(Arrays.toString(a));
+		}
+}
