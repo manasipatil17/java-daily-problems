4684bdba421311fd84021d770e82e0235d49186e
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-23 21:22:13 +0530

    added OneTo100Even.java

---
+++ src/com/prac/oneTo100Even.java
@@ -0,0 +1,13 @@
+package com.prac;
+
+public class OneTo100Even {
+public static void main(String[] args) {
+	System.out.println("Even numbers from 1 to 100 : ");
+	for(int i=1;i<=100;i++) {
+		
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	}
+}
+}
