0678ca68aa7068ffdb739e29b29f9032932ab979
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-28 17:18:54 +0530

    added Anagram.java

---
+++ src/com/string/Anagram.java
@@ -0,0 +1,27 @@
+package com.string;
+
+import java.util.Arrays;
+
+public class Anagram {
+public static void main(String[] args) {
+	String s1="silent";
+	String s2="listen";
+	
+	if(s1.length()==s2.length()) {
+		
+	char[] c1=s1.toCharArray();
+	char[] c2=s2.toCharArray();
+	Arrays.sort(c1);
+	Arrays.sort(c2);
+	if(Arrays.equals(c1,c2)) {
+		System.out.println("String is anagram");
+	}
+	else {
+		System.out.println("String is not anagram");
+	}
+	}
+	else {
+		System.out.println("Strings are not anagram");
+	}
+}
+}
