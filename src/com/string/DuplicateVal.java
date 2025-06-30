ab4e8bd3f48be9bd566bf4d284fdf6635397ad86
Manasi Patil <manasipatil17052004@gmail.com>
2025-06-30 17:28:19 +0530

    added DuplicateVal.java

---
+++ src/com/string/DuplicateVal.java
@@ -0,0 +1,27 @@
+package com.string;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class DuplicateVal {
+public static void main(String[] args) {
+	String s="programming";
+	
+	Map<Character,Integer> m=new HashMap();
+	
+	for(int i=0;i<s.length();i++) {
+		char c=s.charAt(i);
+		if(m.containsKey(c)) {
+			m.put(c, m.get(c)+1);
+		}else {
+		m.put(c , 1 );
+	}
+	}
+	for( Entry<Character,Integer> e: m.entrySet()) {
+	if(e.getValue()>1) {
+		System.out.println(e);
+	}
+}
+}
+}
