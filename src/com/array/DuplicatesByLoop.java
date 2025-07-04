9907d1d7d8eafa6dffdefa5b4ac8ccb66ae50130
Manasi Patil <manasipatil17052004@gmail.com>
2025-07-04 19:36:45 +0530

    Added DuplicatesByLoop.java

--- src/com/array/DuplicatesByLoop.java
+++ src/com/array/DuplicatesByLoop.java
@@ -4,12 +4,13 @@ public class DuplicatesByLoop {
 public static void main(String[] args) {
 	int arr[]= {11,32,12,7,65,43,22,12,32};
 	
+	System.out.println("Duplicate array elements are : ");
 	for(int i=0;i<arr.length;i++) {
 		for(int j=i+1;j<arr.length;j++) {
 			if(arr[i]==arr[j]) {
-		System.out.print(arr[i]+" ");
+				System.out.println(arr[i]);
 			}
+		}
 	}
-}
 }
 }
