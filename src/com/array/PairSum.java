39d5ea646312bee08b25805320855d4fd1bf6f1d
Manasi Patil <manasipatil17052004@gmail.com>
2025-07-04 19:44:44 +0530

    Added PairSum.java

--- src/com/array/PairSum.java
+++ src/com/array/PairSum.java
@@ -3,14 +3,15 @@ package com.array;
 public class PairSum {
 public static void main(String[] args) {
 	int[] a= {1,9,2,7,8,3,4,6};
+
 	int sum=10;
-	System.out.println("Pair with sum 10 :");
+	
 	for(int i=0;i<a.length;i++) {
 		for(int j=i+1;j<a.length;j++) {
-			if(a[i]+a[j]==sum) {
-				System.out.print("("+a[i]+","+a[j]+")");
-			}
+		if(a[i]+a[j]==sum) {
+			System.out.println("("+a[i]+","+a[j]+")");
 		}
 	}
+}
 }
 }
