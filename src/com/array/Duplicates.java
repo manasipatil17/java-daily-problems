5af6f0a808ee3cc137bac843f79395cab6bc339c
Manasi Patil <manasipatil17052004@gmail.com>
2025-07-09 19:19:24 +0530

    added Duplicates.java

--- src/com/array/Duplicates.java
+++ src/com/array/Duplicates.java
@@ -7,10 +7,10 @@ import java.util.Map.Entry;
 public class Duplicates {
 public static void main(String[] args) {
 	int a[]= {12,33,21,55,77,55,44,33};
-	Map<Integer, Integer> m=new HashMap();
+	
+	Map<Integer, Integer> m=new HashMap<>();
 	
 	for(int i=0;i<a.length;i++) {
-		
 		if(m.containsKey(a[i])) {
 			m.put(a[i], m.get(a[i])+1);
 		}
@@ -18,7 +18,7 @@ public static void main(String[] args) {
 			m.put(a[i], 1);
 		}
 	}
-	for(Entry<Integer,Integer> e:m.entrySet()) {
+	for(Entry<Integer,Integer> e: m.entrySet()) {
 		if(e.getValue()>1) {
 			System.out.println(e);
 		}
