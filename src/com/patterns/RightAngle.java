aa8bba35fa17b073d4a88645357993d9295dda2c
Manasi Patil <manasipatil17052004@gmail.com>
2025-07-21 21:44:20 +0530

    added RightAngle.java

--- src/com/patterns/RightAngle.java
+++ src/com/patterns/RightAngle.java
@@ -4,9 +4,9 @@ public class RightAngle {
 public static void main(String[] args) {
 	int n=5;
 	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=i;j++) {
-			System.out.print("* ");
-		}
+	for(int j=0;j<i;j++) {
+		System.out.print("*");
+	}
 		System.out.println();
 	}
 }
