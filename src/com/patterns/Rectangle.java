e99160aaa2a57be826387bd3d922c3acc587a8bb
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-09 22:25:04 +0530

    added rectangle star pattern

--- src/com/patterns/Rectangle.java
+++ src/com/patterns/Rectangle.java
@@ -2,13 +2,14 @@ package com.patterns;
 
 public class Rectangle {
 public static void main(String[] args) {
-	
-int n=5;
-for(int i=0;i<n;i++) {
-	for(int j=0;j<n;j++) {
-		System.out.print("*");
+
+	int n=5;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+		System.out.print(" * ");
 	}
-	System.out.println();
-}
+		System.out.println();
+	}
+
 }
 }
