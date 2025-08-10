9814ff8e6b6ff8d84d3bb0e210c103d3544ce5f6
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-10 22:15:43 +0530

    added date after 500 days

--- src/com/date/DateAfter500days.java
+++ src/com/date/DateAfter500days.java
@@ -8,9 +8,10 @@ public class DateAfter500days {
 	
 	public static void main(String[] args) {
         // Get today's date
-		LocalDate today=LocalDate.now();
-		LocalDate future=today.plusDays(500);
-		System.out.println("Today's Date : "+today);
-  System.out.println("Date After 500 days : "+future);
+	LocalDate today= LocalDate.now();
+	LocalDate future = today.plusDays(500);
+	
+	System.out.println("Today's date : "+today);
+	System.out.println("Date after 500 days : "+future);
 	}
 }
