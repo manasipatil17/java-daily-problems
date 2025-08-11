0bd5d93fdd7fba77d326b80ea1ad85b0dcbd0dc5
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-11 21:45:03 +0530

    added even numbers from array

--- src/com/array/Even.java
+++ src/com/array/Even.java
@@ -8,13 +8,14 @@ public class Even {
 		public static void main(String[] args) {
 
 			int num[]= {11,12,13,14,15,16,17,18,19,20};
+		
 			System.out.println(Arrays.toString(num));
-			System.out.println("Even numbers from given array :");
-			for(int n:num) {
+			System.out.println("Even numbers of an array : ");
+			
+			for(int n : num) {
 				if(n%2==0) {
-					System.out.print(n+" ");
+					System.out.println(n);
 				}
 			}
-
 		}
 }
