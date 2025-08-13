c448c7b4e6b388f425137a791319006b184f88eb
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-13 21:44:57 +0530

    added duplicatesByLoops

--- src/com/array/DuplicatesByLoop.java
+++ src/com/array/DuplicatesByLoop.java
@@ -7,10 +7,10 @@ public static void main(String[] args) {
 	System.out.println("Duplicate array elements are : ");
 	for(int i=0;i<arr.length;i++) {
 	for(int j=i+1;j<arr.length;j++) {
-			if(arr[i]==arr[j]) {
-				System.out.println(arr[i]);
-			}
+		if(arr[i]==arr[j]) {
+			System.out.println(arr[i]);
 		}
+	}
 	}
 }
 }
