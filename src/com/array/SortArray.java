14196e8b9eab6b1fa89e15c87e24a692f857532e
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-16 22:26:22 +0530

    added array sorting

--- src/com/array/SortArray.java
+++ src/com/array/SortArray.java
@@ -5,16 +5,16 @@ public static void main(String[] args) {
 	int a[]= {12,33,22,11,87,66,44};
 	
 	for(int i=0;i<a.length;i++) {
-		for(int j=0;j<a.length-1-i;j++) {
-			
-		if(a[j]>a[j+1]) {
-			int temp = a[j];
-			a[j]=a[j+1];
-			a[j+1]=temp;
+		for(int j=0;j<a.length-i-1;j++) {
+			if(a[j]>a[j+1]) {
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
 		}
 	}
-	}
-	for(int n : a) {
+	
+		for(int n : a) {
 		System.out.print(n+" ");
 	}
 }
