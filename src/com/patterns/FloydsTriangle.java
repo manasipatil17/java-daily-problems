70d31dbf7293168ab9b4e326ffb010dd91e9f3fb
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-26 22:31:42 +0530

    added FolyesTriangle pattern

--- src/com/patterns/FloydsTriangle.java
+++ src/com/patterns/FloydsTriangle.java
@@ -4,9 +4,9 @@ public class FloydsTriangle {
 public static void main(String[] args) {
 	int n=5;
 	int num=1;
-	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=i;j++) {
-			System.out.print(num+" ");
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<i;j++) {
+			System.out.print(num);
 			num++;
 		}
 		System.out.println();
