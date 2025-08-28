d869728e9f0cc758255951ec3fe8f55aaeb889b7
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-28 21:13:54 +0530

    added pyramid

--- src/com/patterns/Pyramid.java
+++ src/com/patterns/Pyramid.java
@@ -3,14 +3,15 @@ package com.patterns;
 public class Pyramid {
 public static void main(String[] args) {
 	int n=5;
-	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=n-i;j++) {
-			System.out.print(" ");
-		}
-		for(int j=1;j<=i;j++) {
-			System.out.print(" *");
-		}
-		System.out.println();
-	}
+    for(int i=1;i<=n;i++) {
+    	 for(int j=1;j<=n-i;j++) {
+    		 System.out.print(" ");
+    	 }
+    	 for(int j=1;j<=i;j++) {
+    		 System.out.print("* ");
+    	 }
+    	 System.out.println();
+    }
+    
 }
 }
