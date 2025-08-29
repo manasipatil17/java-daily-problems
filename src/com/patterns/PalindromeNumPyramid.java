48fa53fc9abc073e77c7e7b3b346ccf588e07336
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-29 21:07:50 +0530

    added hollow rectangle

--- src/com/patterns/PalindromeNumPyramid.java
+++ src/com/patterns/PalindromeNumPyramid.java
@@ -3,8 +3,8 @@ package com.patterns;
 public class PalindromeNumPyramid {
 public static void main(String[] args) {
 	int n=5;
-	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=n-i;j++) {
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n-i;j++){
 			System.out.print("  ");
 		}
 		for(int j=i;j>=1;j--) {
--- src/com/patterns/Pyramid.java
+++ src/com/patterns/Pyramid.java
@@ -11,7 +11,6 @@ public static void main(String[] args) {
     		 System.out.print("* ");
     	 }
     	 System.out.println();
-    }
-    
+    }   
 }
 }
