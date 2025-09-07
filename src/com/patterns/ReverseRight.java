e63d0b28536f60dcbd1915b337c479aa7182009f
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-07 21:32:34 +0530

    added pyrammid reverse

--- src/com/patterns/ReverseRight.java
+++ src/com/patterns/ReverseRight.java
@@ -4,9 +4,9 @@ public class ReverseRight {
 public static void main(String[] args) {
 	
 	for(int i=5;i>=1;i--) {
-		for(int j=1;j<=i;j++) {
-			System.out.print(" * ");
-		}
+	for(int j=1;j<=i;j++) {
+		System.out.print("* ");
+	}
 		System.out.println();
 	}
 	}
