69c2c76549f221cb6ec604414329da0ac7d9532b
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-10 19:04:45 +0530

    added count of digits

--- src/com/prac/CountDight.java
+++ src/com/prac/CountDight.java
@@ -2,12 +2,13 @@ package com.prac;
 
 public class CountDight {
 public static void main(String[] args) {
-	int number=32456;
-	int count=0;
-	while(number!=0) {
-		count++;
-		number=number/10;
-	}
-	System.out.println("Total digits : "+count);
+int num=1234522;
+int count = 0;
+ while(num>0) {
+	 count++;
+	 num/=10;
+	 
+ }
+ System.out.println("count of digits present : "+count);
 }
 }
