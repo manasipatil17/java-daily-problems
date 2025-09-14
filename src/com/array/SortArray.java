23982f484d622d4edeee474a61abc839819fa4a2
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-14 18:26:41 +0530

    added array sorting

--- src/com/array/SortArray.java
+++ src/com/array/SortArray.java
@@ -7,13 +7,12 @@ public static void main(String[] args) {
 	for(int i=0;i<a.length;i++) {
 		for(int j=0;j<a.length-i-1;j++) {
 			if(a[j]>a[j+1]) {
-				int temp=a[j];
+				int temp =a[j];
 				a[j]=a[j+1];
 				a[j+1]=temp;
 			}
 		}
 	}
-	
 		for(int n : a) {
 		System.out.print(n+" ");
 	}
