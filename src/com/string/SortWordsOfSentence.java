9b4d3b11a335ab332d12720da79b8190bcafc069
Manasi Patil <manasipatil17052004@gmail.com>
2025-09-29 18:29:03 +0530

    added SortWordsOfSentence.java

---
+++ src/com/string/SortWordsOfSentence.java
@@ -0,0 +1,21 @@
+package com.string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortWordsOfSentence {
+
+	public static void main(String[] args) {
+		String s;
+		System.out.println("Enter sentence : ");
+		Scanner sc= new Scanner(System.in);
+		s=sc.nextLine();
+		
+		String a[]=s.split(" ");
+		Arrays.sort(a);
+		
+		for (String s1 : a) {
+			System.out.print(s1+" ");
+		}
+	}
+}
