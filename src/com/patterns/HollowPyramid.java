97f91b95f431902947180fb9d07b96d1ec209bde
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-05 13:50:05 +0530

    added hollow pyramid pattern

--- src/com/patterns/HollowPyramid.java
+++ src/com/patterns/HollowPyramid.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 		}
 		for(int j=1;j<=i;j++) {
 			if(j==1||j==i||i==n) {
-			System.out.print("* ");
+				System.out.print("* ");
 			}
 			else {
 				System.out.print("  ");
