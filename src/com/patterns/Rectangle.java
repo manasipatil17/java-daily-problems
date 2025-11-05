6fe15d775a5bb82c748878643111e1e72fa976e9
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-05 12:23:48 +0530

    added rectangle  pattern

--- src/com/patterns/Rectangle.java
+++ src/com/patterns/Rectangle.java
@@ -2,13 +2,12 @@ package com.patterns;
 
 public class Rectangle {
 public static void main(String[] args) {
-
-	int n=5;
-	for(int i=0;i<n;i++) {
-		for(int j=0;j<n;j++) {
-			System.out.print("*");
-		}
-		System.out.println();
+int n=5;
+for(int i=1; i<=n;i++) {
+	for(int j=1;j<=n;j++) {
+		System.out.print("* ");
 	}
+	System.out.println();
+}
 }
 }
