2f058fa63a4007f1320cd445f7bca6cd4cf5ade8
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-05 12:17:40 +0530

    added rhombus pattern

--- src/com/patterns/Rhombus.java
+++ src/com/patterns/Rhombus.java
@@ -2,13 +2,13 @@ package com.patterns;
 
 public class Rhombus {
 public static void main(String[] args) {
-	int n=5;
-	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=n-i;j++) {
+	int num=5;
+	for(int i=1;i<=num;i++) {
+		for(int j=1;j<=num-i;j++) {
 			System.out.print(" ");
 		}
-		for(int j=1;j<=n;j++) {
-			System.out.print("* ");
+		for(int j=1;j<=num;j++) {
+			System.out.print("*");
 		}
 		System.out.println();
 	}
