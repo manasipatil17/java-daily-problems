cf9932e80d95304e14ddead356e5e21d6d17f72e
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-05 13:46:48 +0530

    added hollow rectangle pattern

--- src/com/patterns/HollowRectangle.java
+++ src/com/patterns/HollowRectangle.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	int n=5;
 	for(int i=1;i<=n;i++) {
 		for(int j=1;j<=n;j++) {
-			if(i==1||j==1||i==n||j==n) {
+			if(i==1||j==1||j==n||i==n) {
 				System.out.print("* ");
 			}
 			else {
