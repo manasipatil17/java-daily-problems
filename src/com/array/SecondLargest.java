652a6f34bd1988f2d52b19397378deeb885e018d
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-06 11:22:00 +0530

    added program second largest num array

--- src/com/array/SecondLargest.java
+++ src/com/array/SecondLargest.java
@@ -2,19 +2,22 @@ package com.array;
 
 public class SecondLargest {
 public static void main(String[] args) {
-	int arr[]= {23,12,54,33,76,43};
-	int largest= Integer.MIN_VALUE;
-	int secLargest= Integer.MIN_VALUE;
-	
-	for(int n:arr) {
-		if(n>largest) {
-			secLargest=largest;
-			largest=n;
-		}
-		else if(n>secLargest && n!=largest) {
-			secLargest = n;
-		}
-	}
-	System.out.println("Second largest element is "+secLargest);
+	int a[]= {23,12,54,33,76,43};
+    int largest=Integer.MIN_VALUE;
+    int secLargest=Integer.MIN_VALUE;
+    
+    for(int i=0;i<a.length;i++) {
+    	int temp;
+    	if(a[i]>largest) {
+    		temp=largest;
+    		largest=a[i];
+    		secLargest=temp;
+    	}
+    	else if(a[i]<largest && a[i]>secLargest) {
+    		secLargest=a[i];
+    	}
+    	
+    }
+    System.out.println("Second largest element is "+secLargest);
 }
 }
