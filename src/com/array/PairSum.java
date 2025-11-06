16fba6133bb092441d6e4cbb4deb0919c8366953
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-06 11:31:11 +0530

    added pair sum in array

--- src/com/array/PairSum.java
+++ src/com/array/PairSum.java
@@ -5,14 +5,12 @@ public static void main(String[] args) {
 	int[] a= {1,9,2,7,8,3,4,6};
 
 	int sum=10;
-	
-	for(int i=0;i<=a.length;i++) {
+	for(int i=0;i<a.length;i++) {
 		for(int j=i+1;j<a.length;j++) {
 			if(a[i]+a[j]==sum) {
-				System.out.println(a[i]+" + "+a[j]+" = "+sum);
+				System.out.println(a[i]+"+"+a[j]+"="+sum);
 			}
 		}
 	}
-	
 }
 }
