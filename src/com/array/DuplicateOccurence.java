b3bbbd61f8acefe0b2809f6d1e889c6ef82e79cc
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-06 11:35:26 +0530

    added occurence in array

--- src/com/array/DuplicateOccurence.java
+++ src/com/array/DuplicateOccurence.java
@@ -12,13 +12,14 @@ public static void main(String[] args) {
 	n=sc.nextInt();
 	
 	int a[]= {12,3,44,2,55,4,3,22};
-	int c=0;
-	for(int i=0;i<a.length;i++) {
-		if(a[i]==n) {
-			c++;
-		}
-	}
-	System.out.println("appearance of "+n+" is " +c+ " times");
+     int count =0;
+     for(int i=0;i<a.length;i++) {
+    	 if(a[i]==n) {
+    		 count++;
+    	 }
+     }
+     System.out.println("Count of "+n+" in array is "+count);
+
 }
 
 }
--- src/com/array/MinMaxInArray.java
+++ src/com/array/MinMaxInArray.java
@@ -6,19 +6,18 @@ public class MinMaxInArray {
 	public static void main(String[] args) {
 		int a[]= {11,23,1,44,67,33};
 		
-		int min=a[0];
-		int max=a[0];
-		
-		for(int i=0;i<a.length;i++) {
-			if(a[i]>max) {
-				max=a[i];
-			}
-			else if(a[i]<min) {
-				min=a[i];
-			}
+	int max=a[0];
+	int min=a[0];
+	
+	for(int i=0;i<a.length;i++) {
+		if(a[i]>max) {
+			max=a[i];
 		}
-		System.out.println("Maximum element : "+max);
-		System.out.println("Minimum element : "+min);
-		
+		else if(a[i]<min) {
+			min=a[i];
+		}
+	}
+	System.out.println("Largest number : "+max);
+	System.out.println("Smallest number : "+min);
 	}
 }
