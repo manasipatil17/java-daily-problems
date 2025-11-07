8f238a7e03a7c4e6fe574eab8473d95025dab8c6
Manasi Patil <manasipatil17052004@gmail.com>
2025-11-07 17:33:56 +0530

    added AcmeQues.java

--- src/com/patterns/AcmeQues.java
+++ src/com/patterns/AcmeQues.java
@@ -2,27 +2,24 @@ package com.patterns;
 
 public class AcmeQues {
 public static void main(String[] args) {
-	int n=7;
-	int c=20;
-	
-	for(int i=1;i<=n;i++) {
-	int row=i;
-	if(i%2==0) {
-	for(int j=1;j<=i;j++) {
-		System.out.print(c);
-		c+=5;
-	}
-	}
-	else {
-        // For odd rows, print stars equal to row number - 1
-        int stars = (i == 1) ? 1 : i;
-        for (int j = 1; j <= stars; j++) {
-            System.out.print("* ");
-        }
-    }
 
+	int n=7;
+	for(int i=1;i<=n;i++) {
+		int num=i*10;
+		for(int j=1;j<=i;j++) {
+		if(i==1 && j==1) {
+			System.out.print("*");
+		}
+		else if( i%2==0 || j==1) {
+			System.out.print(num);
+		}
+		else {
+			System.out.print(" *");
+		}
+		num+=5;
+	}
 	System.out.println();
-	}
-	}
-	
+
+}
+}
 }
